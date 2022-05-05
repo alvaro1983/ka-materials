1193d802a40ff8979a054fe4ab8ef849b8d1c597
Alvaro Ruiz Ray <alvaro.ruiz.ray@gmail.com>
2022-05-05 23:54:00 +0200

    Use of Lambda instead of function Parameter

--- 21-functional-programming/projects/starter/src/main/kotlin/main.kt
+++ 21-functional-programming/projects/starter/src/main/kotlin/main.kt
@@ -31,10 +31,7 @@
 fun main() {
     val firstRobot = Robot("Experimental Space Navigation Droid")
     val secondRobot = Robot("Extra-Terrestrial Air Safety Droid")
-    Battlefield
-        .beginBattle(firstRobot, secondRobot, ::onBattleEnded)
-}
-
-fun onBattleEnded(winner: Robot) {
-    winner.report("Win!")
+    Battlefield.beginBattle(firstRobot, secondRobot) {
+        it.report("Win!")
+    }
 }
